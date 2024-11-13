f28ee4535324d4591634c597d565ebf3df72d825
hollomyfoolish <allen.gong@sap.com>
2024-11-13 09:32:06 +0800

    add dependency of slf4j

--- src/main/java/com/allen/test/dependabot/Main.java
+++ src/main/java/com/allen/test/dependabot/Main.java
@@ -1,9 +1,14 @@
 package com.allen.test.dependabot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     // generate a unit test for this class
     public static void main(String[] args) {
+        logger.info("Hello World!");
         System.out.println("Hello World!");
     }
 
