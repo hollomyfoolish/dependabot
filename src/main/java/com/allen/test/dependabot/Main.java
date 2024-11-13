766370df748da3b571dd0ac1a168a4ca930fc94c
Allen Gong <allen.gong@sap.com>
2024-11-13 09:18:05 +0800

    init project

---
+++ src/main/java/com/allen/test/dependabot/Main.java
@@ -0,0 +1,10 @@
+package com.allen.test.dependabot;
+
+public class Main {
+
+    // generate a unit test for this class
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
